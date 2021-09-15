a5cc038868c272d0bf80ba348db1e4846c1e1574
Dmaina5054 <dmain5054@gmail.com>
2021-09-15 11:07:40 +0300

    get covid19 summaries

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,19 +11,21 @@ export class DashboardComponent implements OnInit {
 
   title = 'Consume api Angular'
   summaries: any; //data holder for returned items
-  // We use this trigger because fetching the list of persons can be quite long,
+  // We use this trigger because fetching the list of summaries can be quite long,
   // thus we ensure the data is fetched before rendering
   dtTrigger: Subject<any> = new Subject<any>();
-
-
-
+  dtOptions: DataTables.Settings = {};
 
 //injecting the service into our constructor
 constructor(private httpService: CovidgetterService) {
 
 }
-
 ngOnInit() {
+
+  this.dtOptions = {
+    pagingType: 'full_numbers',
+    pageLength: 2
+  }
   //when component loaded
   //please fetch data from api
   this.httpService.getSummaries().subscribe(
